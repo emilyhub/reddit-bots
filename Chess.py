af6deeb7c4ab69346e8c85d510674b851a949873
em <emilyl@berkeley.edu>
2018-01-25 10:45:30 -0800

    Added valid moves for bishop, rook, knight, and queen

--- Chess.py
+++ Chess.py
@@ -14,57 +14,89 @@ import re
 #     print("Logged in")
 #     return r
 #
-# def run_bot(r, comments_replied_to):
-#     print("Getting the 25 newest comments")
-#     for comment in r.subreddit("test").comments(limit=25):
-#         if "Chess challenge" in comment.body and comment.id not in comments_replied_to:
-#             print("\"Chess challenge\" found in comment " + comment.id)
-#             comment_reply = "|A|B|C|D|E|F|G|H|\n
-#                             :-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n
-#                             1|♖|♘|♗|♕|♔|♗|♘|♖|\n
-#                             2|♙|♙|♙|♙|♙|♙|♙|♙|\n
-#                             3|||||||||\n
-#                             4|||||||||\n
-#                             5|||||||||\n
-#                             6|||||||||\n
-#                             7|♟|♟|♟|♟|♟|♟|♟|♟|\n
-#                             8|♜|♞|♝|♛|♚|♝|♞|♜​|\n"
-#             comment.reply(comment_reply)
-#             print("Replied to comment " + comment.id)
-#         elif
-#
-#             comments_replied_to.append(comment.id)
-#             with open("comments_replied_to.txt", "a") as f:
-#                 f.write(comment.id + "\n")
+def run_bot(r, comments_replied_to):
+    print("Getting the 25 newest comments")
+    for comment in r.subreddit("test").comments(limit=25):
+        if "Chess challenge" in comment.body and comment.id not in comments_replied_to:
+            print("\"Chess challenge\" found in comment " + comment.id)
+            comment.reply(format(default))
+            print("Replied to comment " + comment.id)
 
-# def move(comment, comments_replied_to):
-#     for reply in comments.replies:
-#         if not "Move" in comment.body:
-#             comment.reply("Include \'move\' in your reply")
-#         if reply.id not in comments_replied_to:
-#             # Reply should be in the form of "Move [piece] from [start] to [destination]"
-#             reply = reply.split()
-#             piece = reply[1]
-#             start = reply[3]
-#             destination = reply[5]
-#             print("Trying to move ", piece, " from ", start, " to ", destination)
-#             comment_reply = "
-#                             1|♖|♘|♗|♕|♔|♗|♘|♖|\n
-#                             2|♙|♙|♙|♙|♙|♙|♙|♙|\n
-#                             3|||||||||\n
-#                             4|||||||||\n
-#                             5|||||||||\n
-#                             6|||||||||\n
-#                             7|♟|♟|♟|♟|♟|♟|♟|♟|\n
-#                             8|♜|♞|♝|♛|♚|♝|♞|♜​|\n"
-#             comment.reply(comment_reply)
-#             print("Replied to comment " + comment.id)
-#         comments_replied_to.append(comment.id)
-#         with open("comments_replied_to.txt", "a") as f:
-#             f.write(comment.id + "\n")
+            #Add to comments_replied_to
+            comments_replied_to.append(comment.id)
+            with open("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
+
+def move(comment, comments_replied_to):
+    for reply in comments.replies:
+        if not "Move" in comment.body:
+            comment.reply("Include \'move\' in your reply")
+        if reply.id not in comments_replied_to:
+            # Reply should be in the form of "Move [piece] from [start] to [destination]"
+            reply = reply.split()
+            piece = reply[1]
+            start = reply[3]
+            destination = reply[5]
+            print("Trying to move ", piece, " from ", start, " to ", destination)
+            comment_reply = ""
+            comment.reply(comment_reply)
+            print("Replied to comment " + comment.id)
+
+        # Add to comments_replied_to
+        comments_replied_to.append(comment.id)
+        with open("comments_replied_to.txt", "a") as f:
+            f.write(comment.id + "\n")
+
+# Start and destination are in the form [row, column]
+def isValid(board, piece, start, destination):
+    if destination[0] < 0 or destination[0] > 7 or destination[1] < 0 or destination[1] > 7:
+        print("Invalid destination")
+        return False
+
+    if start[0] == destination[0] and start[1] == destination[1]:
+        print("Please make a move")
+
+    if piece == rook:
+        if start[0] != destination[0] and start[1] != destination[1]:
+            print("Please move horizontally or vertically")
+            return False
+        if start[0] == destination[0]:
+            if not all(p == " " for p in board[start[0]][start[1] + 1 : destination[1]]):
+                print("There is something in the row blocking your rook's movement")
+                return False
+        if start[1] == destination[1]:
+            if not all(p == " " for p in zip*(board)[start[0]][start[1] + 1 : destination[1]]):
+                print("There is something in the column blocking your rook's movement")
+                return False
+
+    if piece == knight:
+        possible = [
+            [1, 2],
+            [2, 1],
+        ]
+        if [abs(destination[0] - start[0]), abs(destination[1] - start[1])] in possible:
+            return True
+        else:
+            print("Invalid knight movement")
+            return False
+
+    if piece == bishop:
+        if abs(start[0] - destination[0]) != abs(start[1] - destination[1]):
+            print("You must move your bishop diagonally")
+            return False
+        vertical = 1 if destination[0] > start[0] else vertical = -1
+        horizontal = 1 if destination[1] > start[1] else horizontal = -1
+
+    if piece == queen:
+        if isValid(board, bishop, start, destination) or isValid(board, rook, start, destination):
+            print("You must move your queen either diagonally, vertically, or horizontally")
+            return False
+        else:
+            return True
+
 
 # Converts arrays of pieces to Reddit markdown
-def printBoard(board):
+def format(board):
     blackIcons = {
         "rook": "♖",
         "knight": "♘",
@@ -105,7 +137,7 @@ def get_saved_comments():
             comments_replied_to = comments_replied_to.split("\n")
     return comments_replied_to
 
-array = [["brook", "bknight", "bbishop", "bqueen", "bking", "bbishop", "bknight", "brook"],
+newGame = [["brook", "bknight", "bbishop", "bqueen", "bking", "bbishop", "bknight", "brook"],
          ["bpawn"]*8,
          [" "]*8,
          [" "]*8,
@@ -114,7 +146,6 @@ array = [["brook", "bknight", "bbishop", "bqueen", "bking", "bbishop", "bknight"
          ["wpawn"]*8,
          ["wrook", "wknight", "wbishop", "wqueen", "wking", "wbishop", "wknight", "wrook"]
          ]
-print(printBoard(array))
 
 
 # r = bot_login()
