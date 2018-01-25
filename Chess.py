d06a4b7bcad7a2599904882f2238ca74f0569c2f
em <emilyl@berkeley.edu>
2018-01-24 23:44:12 -0800

    A text chess game for single threads

---
+++ Chess.py
@@ -0,0 +1,94 @@
+import praw, config
+import requests, os
+import re
+
+def bot_login():
+    r = praw.Reddit(username = config.username,
+                password = config.password,
+                client_id = config.client_id,
+                client_secret = config.client_secret,
+                user_agent = "Text chess")
+    print("Logged in")
+    return r
+
+def run_bot(r, comments_replied_to):
+    print("Getting the 25 newest comments")
+    for comment in r.subreddit("test").comments(limit=25):
+        if "Chess challenge" in comment.body and comment.id not in comments_replied_to:
+            print("\"Chess challenge\" found in comment " + comment.id)
+            comment_reply = "|A|B|C|D|E|F|G|H|\n
+                            :-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n
+                            1|♖|♘|♗|♕|♔|♗|♘|♖|\n
+                            2|♙|♙|♙|♙|♙|♙|♙|♙|\n
+                            3|||||||||\n
+                            4|||||||||\n
+                            5|||||||||\n
+                            6|||||||||\n
+                            7|♟|♟|♟|♟|♟|♟|♟|♟|\n
+                            8|♜|♞|♝|♛|♚|♝|♞|♜​|\n"
+            comment.reply(comment_reply)
+            print("Replied to comment " + comment.id)
+        elif
+
+            comments_replied_to.append(comment.id)
+            with open("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
+
+def move(comment, comments_replied_to):
+    for reply in comments.replies:
+        if not "Move" in comment.body:
+            comment.reply("Include \'move\' in your reply")
+        if reply.id not in comments_replied_to:
+            # Reply should be in the form of "Move [piece] from [start] to [destination]"
+            reply = reply.split()
+            piece = reply[1]
+            start = reply[3]
+            destination = reply[5]
+            print("Trying to move ", piece, " from ", start, " to ", destination)
+            comment_reply = "
+                            1|♖|♘|♗|♕|♔|♗|♘|♖|\n
+                            2|♙|♙|♙|♙|♙|♙|♙|♙|\n
+                            3|||||||||\n
+                            4|||||||||\n
+                            5|||||||||\n
+                            6|||||||||\n
+                            7|♟|♟|♟|♟|♟|♟|♟|♟|\n
+                            8|♜|♞|♝|♛|♚|♝|♞|♜​|\n"
+            comment.reply(comment_reply)
+            print("Replied to comment " + comment.id)
+        comments_replied_to.append(comment.id)
+        with open("comments_replied_to.txt", "a") as f:
+            f.write(comment.id + "\n")
+
+def printBoard(board, side):
+    blackIcons = {
+        "rook": "♖",
+        "knight": "♘",
+        "bishop": "♗",
+        "queen": "♕",
+        "king": "♔",
+        "pawn": "♙"}
+    whiteIcons = {
+        "rook": "♜",
+        "knight": "♞",
+        "bishop": "♝",
+        "queen": "♛",
+        "king": "♚",
+        "pawn": "♟"
+    }
+    reply += "\|A\|B\|C\|D\|E\|F\|G\|H\|\n
+            :-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n"
+
+def get_saved_comments():
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = f.read()
+            comments_replied_to = comments_replied_to.split("\n")
+    return comments_replied_to
+
+
+r = bot_login()
+comments_replied_to = get_saved_comments()
+run_bot(r, comments_replied_to)
